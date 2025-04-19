a1ed6605772e73a070b0c66bb709450c29800645
prasad.ghole <prasad.ghole@ideaforgetech.com>
2025-04-19 18:24:12 +0530

    Changed ALT to Windows super key to match ubuntu.

---
+++ myalias.sh
@@ -0,0 +1,10 @@
+# My alias
+#!/bin/bash
+
+
+alias grv='git remote -vv'
+alias gst='git status'
+alias ga='git add'
+alias ll='ls -l'
+alias ls='ls -F --color=auto --show-control-chars'
+alias winget='winpty winget.exe'
